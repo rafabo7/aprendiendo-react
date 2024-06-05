29c7fd0e653edf58ba3c0381a1bcba098a8b6d99
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-06-05 17:54:50 +0200

    Learning React. Tic Tac Toe project. A project to play Tic Tac Toe. Stage: advance, unfinished. Topics: conditional rendering, useState() hook, programming logic and good practices.
    Aprendiendo React. Proyecto: Tres en raya.
    Un proyecto que emula el tres en raya.
    Estado: avanzado pero no acabado.
    Temas: renderizado condicional, useState() hook, lógica de programación
    y buenas prácticas.

--- midudev-react/projects/02-tic-tac-toe/src/App.jsx
+++ midudev-react/projects/02-tic-tac-toe/src/App.jsx
@@ -1,38 +1,68 @@
+import './App.css'
+import { useState } from 'react'
+
 const TURNS = {
-  X: "X",
-  O: "O"
+  X: 'X',
+  O: 'O'
 }
 
 const board = Array(9).fill(null)
 
-const Spuare = ({children, updateBoard, index}) => {
-  return (
-    <div className="square">
-        {children}
-    </div>
+const Square = ({ children, isSelected, updateBoard, index}) => {
+  const className = `square ${ isSelected ? 'is-selected' : ''}`
 
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  return (
+    <div onClick={handleClick} className={className} >
+      {children}
+    </div>
   )
 }
 
-export function App() {
+
+function App () {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+  }
 
   return (
-
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
-        {board.map((_, index) => {
-          return (
-            <Spuare 
-            key={index}
-            index={index}
-            >{index}
-            </Spuare>
-          )
-        })}
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
       </section>
-      
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X} >
+          {TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O} 
+        >{TURNS.O}</Square>
+      </section>
     </main>
   )
-  
 }
+
+export default App
