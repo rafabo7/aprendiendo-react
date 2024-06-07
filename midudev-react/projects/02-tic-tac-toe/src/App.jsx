1955f9246dd8eae17d1e54c72a26b80c5381fd6c
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-06-07 19:37:59 +0200

    Progressing with the game. Create checkWinner() function with WINNER_COMB for winner combinations. Add checkWinner() to updateBoard() logic. Create resetGame() for reseting states, and link it with the reset buttons. Add a visual representation of the winner via conditional rendering.

--- midudev-react/projects/02-tic-tac-toe/src/App.jsx
+++ midudev-react/projects/02-tic-tac-toe/src/App.jsx
@@ -21,13 +21,49 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
   )
 }
 
+const WINNER_COMB = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 
 function App () {
   const [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn] = useState(TURNS.X)
 
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMB) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
   const updateBoard = (index) => {
+
+    if (board[index] || winner) return
+
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
@@ -35,11 +71,19 @@ function App () {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
+    const newWinner = checkWinner(newBoard)
+    
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+
+
   }
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}> Reset Game </button>
       <section className="game">
         {
           board.map((_, index) => {
@@ -61,6 +105,33 @@ function App () {
         <Square isSelected={turn === TURNS.O} 
         >{TURNS.O}</Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false
+                  ? 'Emapate'
+                  : 'Ganó ' + winner
+                }
+              </h2>
+
+              <header className="win">
+                { winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>
+                  Empezar de nuevo
+                </button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
+
     </main>
   )
 }
