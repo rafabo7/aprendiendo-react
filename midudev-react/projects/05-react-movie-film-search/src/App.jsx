14af84d829e57ea52577dfa2a738b3c6e1594c04
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-09-11 18:48:56 +0200

    Add event handlers for search input and submit form including basic validation

--- midudev-react/projects/05-react-movie-film-search/src/App.jsx
+++ midudev-react/projects/05-react-movie-film-search/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useRef } from 'react'
 import './App.css'
 import { Movies } from './Components/Movies'
 import { useMovies } from './hooks/useMovies'
@@ -8,18 +9,49 @@ import { useMovies } from './hooks/useMovies'
 
 function App() {  
 
-  const {movies} = useMovies()
+  
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState(null)
+  const {movies, getMovies} = useMovies( {search} )
+
+  const handleSearch = (e) => {
+    const newSearch = e.target.value
+
+    if (newSearch.startsWith(" ")) return
+
+    setSearch(newSearch)
+
+    if (newSearch === "") {
+      setError("La búsqueda está vacía")
+      return
+    }
+    
+    if (newSearch.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres")
+      return
+    }
+
+    setError(null)
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies(search)
+
+  }
 
   return (
     <div className='App'>
     <header>
       <h1>Buscador de películas</h1>
-      <form action="#" className="form">
-        <input placeholder='Star Wars, Avengers, Frank...' />
+      <form onSubmit={handleSubmit} action="#" className="form">
+        <input onChange={handleSearch} value={search} placeholder='Star Wars, Avengers, Frank...' />
         <button type="submit">Buscar</button>
       </form>
     </header>
     <main>
+      { error && <p style={{color: "red"}} >{error}</p>}
 
       <Movies movies={movies} />
 
